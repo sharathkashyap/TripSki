ec41eef6c37acb09592a9d2db5a237d31234486b
shprasad <sharath.prasad@amadeus.com>
2018-07-27 15:53:28 +0530

    Component Commit

--- tripski/src/app/app.module.ts
+++ tripski/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapScreenComponent } from './components/map-screen/map-screen.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { BookingComponent } from './components/booking/booking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapScreenComponent,
+    FiltersComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
